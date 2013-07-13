8d7e280b63a7f40a5aba1e9bc514fc839ff21773
oroshnivskyy <oroshnivsky@gmail.com>
2013-07-13 18:17:49 +0300

    added clear cache

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,21 @@
 <?php
 Route::get('/', ['as' => 'home', 'uses' => 'HomeController@getIndex']);
-Route::resource('login', 'LoginController', ['only'=>['index','store']]);
+Route::resource('login', 'LoginController', ['only' => ['index', 'store']]);
+Route::get(
+    'cache-clear',
+    function () {
+        if (!Auth::check()) {
+            return App::abort(404);
+        }
+        Cache::flush();
+        return Redirect::back();
+    }
+);
 
 Route::group(
     ['prefix' => '{groupUrl}'],
     function () {
         Route::get('/', ['as' => 'product_group', 'uses' => 'ProductGroupController@show']);
-        Route::get('{productUrl}', ['as'=>'product', 'uses'=>'ProductController@show']);
+        Route::get('{productUrl}', ['as' => 'product', 'uses' => 'ProductController@show']);
     }
 );
---
+++ app/views/admin/dashboard.blade.php
@@ -0,0 +1,1 @@
+<a href="{{url('cache-clear')}}">>>>>>Clear cache<<<<<<</a>
